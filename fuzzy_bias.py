b3772ef5828f3b9ae8dad4a91cc6bc800d03263b
kiran iyer <iyerkiran@live.com>
2016-03-20 23:52:27 +0530

    updated fuzzy_bias

--- fuzzy_bias.py
+++ fuzzy_bias.py
@@ -11,6 +11,22 @@ def conversion_success_rate(data_set):    # data_set is the list which contains 
 
 		success_percentage = fuzz.ratio(english_to_hindi, e[1])    #compares the two text and returns the success rate
 
-		return success_percentage
+		print success_percentage
 
 
+def partial_ratio_rate(data_set):
+	for e in data_set:
+		english_to_hindi = kirans_function(e[0])
+		partial_ratio_percentage = fuzz.partial_ratio(english_to_hindi, e[1])
+
+		print partial_ratio_percentage        #prints the partial ratio % between the two input text
+
+
+#Returns 'Match!' is the conversion success rate is 100%
+
+def print_ans(data_set):
+	for e in data_set:
+		english_to_hindi = kirans_function(e[0])
+		
+		if not [word for word in english_to_hindi.split(' ') if word not in e[1].split(' ')]:
+    		print 'Match found!'             
