b6e4ae30cd729dc017876116a221046a2196aa36
kiran iyer <iyerkiran@live.com>
2016-04-02 22:48:11 +0530

    dual conversion support added

--- main_app.py
+++ main_app.py
@@ -6,6 +6,7 @@ from flask import Flask
 import support_library
 from flask import Flask, render_template, request, make_response
 import nltk 
+import langid
 app = Flask(__name__)
 
 @app.route('/')
@@ -20,10 +21,17 @@ def hello():
     print tokens
     pos_tagging = nltk.pos_tag(tokens)
     print pos_tagging
-    quit()
     client = support_library.MicrosoftTranslatorClient('machinetranslationmanishrana','mjbYdQ3SyROItdT1gJAXUcIxYlBDaEKs3oKZ8XcFq0w=')
-    translated = client.TranslateText(name, 'en', 'hi')
+    language = langid.classify(name)
+    print "printing language........."
+    print language[0]
+    if language[0] == 'en':    
+        translated = client.TranslateText(name, 'en', 'hi')
+    else:
+        translated = client.TranslateText(name, 'hi', 'en')
     translated = translated.replace('"',"")
+    print "translated is"
+    print translated
     return render_template('index.html', name=name, translated=translated)
 
 
