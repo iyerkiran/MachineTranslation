3f2e7bcdca87ed4db778987380f0a21b71fa0e5e
kiran iyer <iyerkiran@live.com>
2016-04-03 11:33:36 +0530

    UI modifications done

--- main_app.py
+++ main_app.py
@@ -5,7 +5,7 @@ sys.setdefaultencoding('utf8')
 from flask import Flask
 import support_library
 from flask import Flask, render_template, request, make_response
-import nltk 
+#import nltk 
 import langid
 app = Flask(__name__)
 
@@ -17,22 +17,26 @@ def home():
 def hello():
     name=request.form['inputtext']
     #connect to the microsoft cloud for searching the various corpora of the english language with their corrosponding hindi corpora.
-    tokens = tokens = nltk.word_tokenize(name)
-    print tokens
-    pos_tagging = nltk.pos_tag(tokens)
-    print pos_tagging
+    #tokens = tokens = nltk.word_tokenize(name)
+    #print tokens
+    #pos_tagging = nltk.pos_tag(tokens)
+    #print pos_tagging
     client = support_library.MicrosoftTranslatorClient('machinetranslationmanishrana','mjbYdQ3SyROItdT1gJAXUcIxYlBDaEKs3oKZ8XcFq0w=')
     language = langid.classify(name)
     print "printing language........."
     print language[0]
     if language[0] == 'en':    
         translated = client.TranslateText(name, 'en', 'hi')
+        input_detected = "English"
+        output_detected = "Hindi"
     else:
         translated = client.TranslateText(name, 'hi', 'en')
+        input_detected = "Hindi"
+        output_detected = "English"
     translated = translated.replace('"',"")
     print "translated is"
     print translated
-    return render_template('index.html', name=name, translated=translated)
+    return render_template('index.html', name=name, translated=translated, input_detected=input_detected,output_detected=output_detected)
 
 
 if __name__ == '__main__':
