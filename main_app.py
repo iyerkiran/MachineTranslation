b25c647f0173956a962da07f917da9ff0af8e380
Iyer <kiran.r.iyer@accenture.com>
2018-07-15 17:34:10 +0530

    Final check in

--- main_app.py
+++ main_app.py
@@ -6,9 +6,12 @@ from flask import Flask
 import support_library
 from flask import Flask, render_template, request, make_response
 #import nltk 
-import langid
+#import langid
+import mtranslate
+from googletrans import Translator
 from Data_set import data_set
 app = Flask(__name__)
+translator = Translator()
 
 @app.route('/')
 def home():
@@ -16,17 +19,34 @@ def home():
 
 @app.route('/', methods=['POST'])
 def hello():
-    name=request.form['inputtext']
+    translator = Translator()
+    input_src=request.form['inputtext']
+    print input_src
+    #b=mtranslate.translate(name,"hi","utf8")
+    #b=b.encode('utf-8')
+    #print b.encode("utf-8")
+    
+    detected = translator.detect(input_src)
+
+    if detected.lang == "hi":
+        input_detected = "Hindi"
+        output_detected = "English"
+        destination_code = "en"
+    if detected.lang == "en":
+        input_detected = "English"
+        output_detected = "Hindi"
+        destination_code = "hi"
+     
+    output_src = translator.translate(input_src, src=detected.lang, dest=destination_code)    
+    print output_src.text
     #connect to the microsoft cloud for searching the various corpora of the english language with their corrosponding hindi corpora.
     #tokens = tokens = nltk.word_tokenize(name)
     #print tokens
     #pos_tagging = nltk.pos_tag(tokens)
     #print pos_tagging
-    client = support_library.MicrosoftTranslatorClient('machinetranslationmanishrana','mjbYdQ3SyROItdT1gJAXUcIxYlBDaEKs3oKZ8XcFq0w=')
-    language = langid.classify(name)
-    print "printing language........."
-    print language[0]
-    if language[0] == 'en':    
+    #client = support_library.MicrosoftTranslatorClient('machinetranslationmanishrana','mjbYdQ3SyROItdT1gJAXUcIxYlBDaEKs3oKZ8XcFq0w=')
+    #language = langid.classify(name)
+    '''if language[0] == 'en':    
         translated = client.TranslateText(name, 'en', 'hi')
         input_detected = "English"
         output_detected = "Hindi"
@@ -37,10 +57,12 @@ def hello():
         input_detected = "Hindi"
         output_detected = "English"
         translated = translated.replace('"',"")
-        data_set.append((translated, name))
-    print "translated is " + translated
-    return render_template('index.html', name=name, translated=translated, input_detected=input_detected,output_detected=output_detected)
-
+        data_set.append((translated, name))'''
+    #output_detected="tst"
+    #translated=name
+    #input_detected="test"
+    return render_template('index.html', name=input_src, translated=output_src.text, input_detected=input_detected,output_detected=output_detected)
+    
 
 if __name__ == '__main__':
     app.run(debug=True) #this ensures auto reloading of the server on detecting any change in the backend.
